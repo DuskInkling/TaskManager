66ced36abc58cf5086754dfc7eeec750ede5b12a
DuskInkling <ggalaxycristi@gmail.com>
2026-04-30 13:26:20 +0300

    Implemented logic for TaskService.cs

--- TaskManager.BusinessLogic/TaskService.cs
+++ TaskManager.BusinessLogic/TaskService.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.VisualBasic;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,9 +12,60 @@ namespace TaskManager.BusinessLogic
     {
         private List<TaskItem> tasks = new List<TaskItem>();
         private int nextId = 1;
-        public void AddTask() { }
-        public void RemoveTask(int id) { }
-        public void UpdateTask(int id) { }
+        public void AddTask(TaskItem task) {
+            task.Id = nextId++;
+            tasks.Add(task);
+        }
+        public void RemoveTask(int id) {
+            var task = tasks.FirstOrDefault(x => x.Id == id);
+            if (task != null)
+            {
+                tasks.Remove(task);
+            }
+        }
+        public void UpdateTask(int id, TaskItem updated) {
+            var task = tasks.FirstOrDefault(x=> x.Id == id);
+            if (task != null) { 
+                task.Title = updated.Title;
+                task.Description = updated.Description;
+                task.Deadline = updated.Deadline;
+                task.Priority = updated.Priority;
+                task.Category = updated.Category;
+                task.State = updated.State;
+            }
+        }
         public List<TaskItem> GetAllTasks() { return tasks; }
+        public TaskItem? GetTaskById(int id) => 
+            tasks.FirstOrDefault(x => x.Id == id);
+        public List<TaskItem> FilterByState (State state) => 
+            tasks.Where(x => x.State == state).ToList();
+        public List<TaskItem> FilterByPriority(Priority priority) => 
+            tasks.Where(x => x.Priority == priority).ToList();
+        public List<TaskItem> FilterByCategory(Category category) => 
+            tasks.Where(t => t.Category == category).ToList();
+        public List<TaskItem> Search(string query) =>
+            tasks.Where(x =>
+            x.Title.Contains(query, StringComparison.OrdinalIgnoreCase) ||
+            x.Description.Contains(query, StringComparison.OrdinalIgnoreCase)).
+            ToList();
+        public List<TaskItem> SortByDate(bool ascending = true) =>
+            ascending
+            ? tasks.OrderBy(x => x.CreationDate).ToList() 
+            : tasks.OrderByDescending(x => x.CreationDate).ToList();
+        public List<TaskItem> SortByDeadline(bool ascending = true) =>
+            ascending
+            ? tasks.OrderBy(x => x.Deadline).ToList()
+            : tasks.OrderByDescending(x => x.Deadline).ToList();
+        public List<TaskItem> GetExpiringTasks(int withinDays = 3) =>
+            tasks.Where(x =>
+            x.State != State.Completed &&
+            x.Deadline >= DateTime.Now &&
+            x.Deadline <= DateTime.Now.AddDays(withinDays))
+            .OrderBy(x => x.Deadline).ToList();
+        public void LoadTasks(List<TaskItem> loadedTasks)
+        {
+            tasks = loadedTasks;
+            nextId = tasks.Count > 0 ? tasks.Max(x => x.Id) + 1 : 1;
+        }
     }
 }

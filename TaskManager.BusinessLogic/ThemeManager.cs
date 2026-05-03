dbc56b8cee0ef89657f6ca411c8afe819b2a76a9
DuskInkling <ggalaxycristi@gmail.com>
2026-05-03 16:24:26 +0300

    Made ThemeManager class

--- TaskManager.BusinessLogic/ThemeManager.cs
+++ TaskManager.BusinessLogic/ThemeManager.cs
@@ -8,5 +8,15 @@ namespace TaskManager.BusinessLogic
 {
     public class ThemeManager
     {
+        public static bool isDarkTheme { get; private set; } = false;
+
+        public void ToggleTheme()
+        {
+            isDarkTheme = !isDarkTheme;
+        }
+        public static string GetCurrentTheme()
+        {
+            return isDarkTheme ? "Dark theme" : "Light Theme" ;
+        }
     }
 }
